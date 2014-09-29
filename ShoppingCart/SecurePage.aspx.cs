95bf6b2b79a172882ec6c25e9b4654394649ca85
Jeff Chu <regionbbs@gmail.com>
2014-09-29 15:39:46 +0800

    initial commit

---
+++ ShoppingCart/SecurePage.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ShoppingCart
+{
+    public partial class SecurePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.IsAuthenticated)
+                this.lblUsername.Text = HttpContext.Current.User.Identity.Name;
+        }
+
+        protected void cmdSignOut_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect("~/SecurePage.aspx");
+        }
+    }
+}
---
+++ ShoppingCart/SecurePage2.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SecurePage2.aspx.cs" Inherits="ShoppingCart.SecurePage2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h2>This is a secure page, login required.</h2>
+        <h5><asp:Label ID="lblUsername" runat="server" /></h5><br />
+        <asp:Button ID="cmdSignOut" runat="server" Text="Logout" OnClick="cmdSignOut_Click" />
+    </div>
+    </form>
+</body>
+</html>
